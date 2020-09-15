a74484628d365900a7c82640220e2920fc3844ce
divdefwork <divdefwork@gmail.com>
2020-09-15 20:32:13 +0300

    Part 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,28 @@
 # -*- coding=utf-8 -*-
 # !/usr/bin/env python3
 
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Привіт, світ!"
+	user = {'username': 'Miguel'}
+	posts = [
+            {
+                'author': {'username': 'John'},
+                'body': 'Beautiful day in Portland!'
+            },
+            {
+                'author': {'username': 'Susan'},
+                'body': 'The Avengers movie was so cool!'
+            },
+            {
+                'author': {'username': 'Іпполіт'},
+                'body': 'Яка гидота ця ваша заливна риба !!'
+            }
+        ]
+
+	return render_template('index.html', title='Головна',
+                        user=user, posts=posts)
