1ea83137a0ef2f958a25e52b94f7d0ac89487d57
divdefwork <divdefwork@gmail.com>
2020-09-16 11:31:40 +0300

    Part 3: Web Forms

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@
 # !/usr/bin/env python3
 
 from flask import Flask
+from config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes  # END
---
+++ app/form.py
@@ -0,0 +1,12 @@
+# -*- coding=utf-8 -*-
+# !/usr/bin/env python3
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Ім'я користувача", validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField("Запам'ятати мене")
+    submit = SubmitField('Увійти')
