0b302fd3c302aee94a23337684c85a89fea7dc73
divdefwork <divdefwork@gmail.com>
2020-09-17 11:47:36 +0300

    format replaced by f

--- app/routes.py
+++ app/routes.py
@@ -34,7 +34,7 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Для користувача потрібен логін {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash(
+            f'Для користувача потрібен логін {form.username.data}, remember_me={form.remember_me.data}')
         return redirect(url_for('index'))
     return render_template('login.html', title='Увійти', form=form)
