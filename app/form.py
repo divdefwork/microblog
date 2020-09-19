2ccb059455364623efed9fc275a852b72ade60a4
divdefwork <divdefwork@gmail.com>
2020-09-19 13:25:10 +0300

    Part 7: Error Handling

--- app/form.py
+++ app/form.py
@@ -41,3 +41,14 @@ class EditProfileForm(FlaskForm):
     username = StringField("Ім'я користувача", validators=[DataRequired()])
     about_me = TextAreaField('Про мене', validators=[Length(min=0, max=140)])
     submit = SubmitField('Відправити')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError(
+                    "Будь ласка, використовуйте інше ім’я користувача.")
