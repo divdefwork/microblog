f4054df910cdc34476d807483503d8d0e73e89d0
divdefwork <divdefwork@gmail.com>
2020-09-20 11:36:55 +0300

    Part 8: End

--- app/routes.py
+++ app/routes.py
@@ -105,3 +105,35 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash(f'Користувача {username} не знайдено.')
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('Ви не можете відслідковувати себе!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash(f'Ви стежите за {username}!')
+    return redirect(url_for('user', username=username))
+
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash(f'Користувача {username} не знайдено.')
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('Ви не можете відписатися від себе!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash(f'Ви не стежите за {username}.')
+    return redirect(url_for('user', username=username))
