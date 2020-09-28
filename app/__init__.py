fd47490e7e5d06a20d6fe0816e96ea35da8b0ff8
divdefwork <divdefwork@gmail.com>
2020-09-28 11:49:58 +0300

    Part 12:  Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 from config import Config
 
 
@@ -21,6 +22,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 login.login_message = "Будь ласка, увійдіть, щоб відкрити цю сторінку."
 mail = Mail(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -40,7 +42,7 @@ if not app.debug:
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, 
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
                                        backupCount=10)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
@@ -49,6 +51,5 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Запуск мікроблогу')
-
 
 from app import routes, models, errors  # END
